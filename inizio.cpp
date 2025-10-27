821cd88b04b0e10f07787db4d26f31197a5b3056
IlConteDiGello <tomilari08@gmail.com>
2025-10-27 09:54:32 +0100

    base

--- inizio.cpp
+++ inizio.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
+
+using namespace std;
+
+void main(){
+    string nomeProdotto;
+
+    int prezzoDiAcquisto;
+
+    int prezzoDiVendita;
+
+    int quantitaInMagazzino;
+}
